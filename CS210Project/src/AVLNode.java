faaae8d36b058533aa1155ccdcf8e20bc5a08f8d
Fares1Albadry <223110256@psu.edu.sa>
2025-11-02 15:13:26 +0300

    AVLNode.java

--- CS210Project/src/AVLNode.java
+++ CS210Project/src/AVLNode.java
@@ -1,3 +1,8 @@
 public class AVLNode {
-
+    private String token ;
+    private int height ;
+    private int frequency ;
+    private SList List;
+    private AVLNode left;
+    private AVLNode right;
 }
