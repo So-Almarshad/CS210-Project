9f3efb903c9806b814d7e8b8baa94837e3351387
Fares1Albadry <223110256@psu.edu.sa>
2025-11-02 15:12:12 +0300

    Node.java

--- CS210Project/src/Node.java
+++ CS210Project/src/Node.java
@@ -1,5 +1,9 @@
 public class Node {
+    private String filename;
+   private int lineNumber;
+   private Node next;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
+
