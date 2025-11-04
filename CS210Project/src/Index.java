e3392460cfef6d6cf9473a75bc60676d39d297d4
Fares AlBadry <223110256@psu.edu.sa>
2025-11-04 06:46:03 +0300

    Index.java

--- CS210Project/src/Index.java
+++ CS210Project/src/Index.java
@@ -3,7 +3,7 @@ public class Index {
     private AVLNode root;
     private int size;
     
-    public Index(){// sheek 3ala line 75 for Node class
+    public Index(){// sheek 3ala line 75 to 82 for Node class , lines 94 106 
         this.root = null;
         this.size = 0;// enta lazem t3el 3lehom
     }
@@ -72,8 +72,52 @@ public class Index {
         return no;
     }
     
-    //public void Insert(String token, String filename, int lineNumber) {
-      //  root = Insert(root, token, filename, lineNumber);
-    //}
+    public void Insert(String token, String filename, int lineNumber) {
+        root = Insert(root, token, filename, lineNumber);
+    }
     //Starting Block 9
+        private AVLNode insert(AVLNode node, String token, String filename, int lineNumber){
+        if (node === null) {//finding the free place,and create new
+            AVLNode newNode = new AVLNode(token);
+            newNode.list.Insert(filename, lineNumber);
+            newNode.frequency =1;
+            size++;
+            //System.out.println("Created new node for: "+token);
+            return newNode;
+        }
+        int comparizon = token.compareTo(node.token);//deciding right or left
+        if (comparizon < 0 ) {//left token smaller
+            node.left = insert(node.left, token, filename, lineNumber);
+        }else if (comparizon > 0){//right token larger
+            node.right = insert(node.right, token , filename, lineNumber);
+        }else{//updating frequency & list, token is already there
+            node.list.Insert(filename, lineNumber);
+            node.frequency++;
+            //System.out.println("Updated existing node: "+token + ", frequency: "+node.frequency);
+            return node;
+        }
+        node.height= Math.max(height(node.left), height(node.right))+1;//height ++
+        return balance(node);
+    }
+    
+    public SList Search(String token){
+        AVLNode node = search(root, token);
+        if (node != null) {
+            return node.list;
+        }
+        return null; //not found
+    }
+    private AVLNode search(AVLNode node, String token){
+        if (node == null) {
+            return null; //not found
+        }
+        int comparizen = token.compareTo(node.token);
+        if (comparizen <0) {//s left
+            return search(node.left,token);
+        }else if(comparizen <0){//s right
+            return search(node.right,token);
+        }else{
+            return node; //found
+        }
+    }
 }
